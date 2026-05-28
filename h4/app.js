f7ab56ab80565e902b0b8214989564ec1a2454a4
sebastian mendoza <smendozaviloria9@gmail.com>
2026-05-28 11:24:18 -0600

    Update app.js

--- h4/app.js
+++ h4/app.js
@@ -1,11 +1,9 @@
 const API_URL = "https://jsonplaceholder.typicode.com/posts";
-
-const taskForm = document.getElementById("productForm");
+const productFormForm = document.getElementById("productForm");
 const productName = document.getElementById("productName");
 const productPrice = document.getElementById("productPrice");
-const taskList = document.getElementById("productList");
+const productList = document.getElementById("productList");
 const syncBtn = document.getElementById("syncBtn");
-
 let productos = [];
 
 function cargarProductos() {
@@ -20,6 +18,8 @@ function cargarProductos() {
     }
 }
 
+
+
 function guardarProductos() {
 
     localStorage.setItem(
@@ -28,23 +28,28 @@ function guardarProductos() {
     );
 }
 
+
+
 function renderProductos() {
 
-    taskList.innerHTML = "";
+
+    productList.innerHTML = "";
+
 
     productos.forEach((producto, index) => {
+
 
         const li = document.createElement("li");
 
         const texto = document.createElement("span");
 
-        texto.textContent = `
-        ${index + 1}. ${producto.nombre} - $${producto.precio}
-        `;
-
+      texto.textContent = `
+      ${index + 1}. ${producto.nombre} - $${producto.precio}
+      `
         const btnEliminar = document.createElement("button");
 
         btnEliminar.textContent = "Eliminar";
+
 
         btnEliminar.addEventListener("click", () => {
 
@@ -56,14 +61,19 @@ function renderProductos() {
 
         li.appendChild(btnEliminar);
 
-        taskList.appendChild(li);
+        productList.appendChild(li);
 
     });
 }
 
-taskForm.addEventListener("submit", async (e) => {
+
+
+
+productForm.addEventListener("submit", async (e) => {
+
 
     e.preventDefault();
+
 
     const nombre = productName.value.trim();
 
@@ -86,6 +96,7 @@ taskForm.addEventListener("submit", async (e) => {
 
     };
 
+
     productos.push(nuevoProducto);
 
     guardarProductos();
@@ -102,16 +113,24 @@ taskForm.addEventListener("submit", async (e) => {
 
 });
 
+
+
 function eliminarProducto(index) {
 
+    // Eliminar del arreglo
     productos.splice(index, 1);
 
+    // Actualizar localStorage
     guardarProductos();
 
+    // Renderizar nuevamente
     renderProductos();
 
     console.log("Producto eliminado");
 }
+
+
+
 
 async function obtenerProductosAPI() {
 
@@ -130,11 +149,13 @@ async function obtenerProductosAPI() {
     }
 }
 
+
+
+
 async function agregarProductoAPI(producto) {
 
     try {
-
-        console.log("Enviando producto");
+        console.log("enviando producto")
 
         const respuesta = await fetch(API_URL, {
 
@@ -160,6 +181,8 @@ async function agregarProductoAPI(producto) {
 
     }
 }
+
+
 
 async function actualizarProductoAPI(id, productoActualizado) {
 
@@ -190,6 +213,9 @@ async function actualizarProductoAPI(id, productoActualizado) {
     }
 }
 
+
+
+
 async function eliminarProductoAPI(id) {
 
     try {
@@ -208,6 +234,253 @@ async function eliminarProductoAPI(id) {
 
     }
 }
+
+
+
+syncBtn.addEventListener("click", () => {
+
+    obtenerProductosAPI();
+
+});
+
+cargarProductos();const API_URL = "https://jsonplaceholder.typicode.com/posts";
+const productFormForm = document.getElementById("productForm");
+const productName = document.getElementById("productName");
+const productPrice = document.getElementById("productPrice");
+const productList = document.getElementById("productList");
+const syncBtn = document.getElementById("syncBtn");
+let productos = [];
+
+function cargarProductos() {
+
+    const datosGuardados = localStorage.getItem("productos");
+
+    if (datosGuardados) {
+
+        productos = JSON.parse(datosGuardados);
+
+        renderProductos();
+    }
+}
+
+
+
+function guardarProductos() {
+
+    localStorage.setItem(
+        "productos",
+        JSON.stringify(productos)
+    );
+}
+
+
+
+function renderProductos() {
+
+
+    productList.innerHTML = "";
+
+
+    productos.forEach((producto, index) => {
+
+
+        const li = document.createElement("li");
+
+        const texto = document.createElement("span");
+
+      texto.textContent = `
+      ${index + 1}. ${producto.nombre} - $${producto.precio}
+      `
+        const btnEliminar = document.createElement("button");
+
+        btnEliminar.textContent = "Eliminar";
+
+
+        btnEliminar.addEventListener("click", () => {
+
+            eliminarProducto(index);
+
+        });
+
+        li.appendChild(texto);
+
+        li.appendChild(btnEliminar);
+
+        productList.appendChild(li);
+
+    });
+}
+
+
+
+
+productForm.addEventListener("submit", async (e) => {
+
+
+    e.preventDefault();
+
+
+    const nombre = productName.value.trim();
+
+    const precio = productPrice.value.trim();
+
+    if (nombre === "" || precio === "") {
+
+        alert("Todos los campos son obligatorios");
+
+        console.error("Campos vacíos");
+
+        return;
+    }
+
+    const nuevoProducto = {
+
+        nombre: nombre,
+
+        precio: precio
+
+    };
+
+
+    productos.push(nuevoProducto);
+
+    guardarProductos();
+
+    renderProductos();
+
+    await agregarProductoAPI(nuevoProducto);
+
+    console.log("Producto agregado");
+
+    productName.value = "";
+
+    productPrice.value = "";
+
+});
+
+
+
+function eliminarProducto(index) {
+
+    // Eliminar del arreglo
+    productos.splice(index, 1);
+
+    // Actualizar localStorage
+    guardarProductos();
+
+    // Renderizar nuevamente
+    renderProductos();
+
+    console.log("Producto eliminado");
+}
+
+
+
+
+async function obtenerProductosAPI() {
+
+    try {
+
+        const respuesta = await fetch(API_URL);
+
+        const datos = await respuesta.json();
+
+        console.log("GET:", datos);
+
+    } catch (error) {
+
+        console.error("Error GET:", error);
+
+    }
+}
+
+
+
+
+async function agregarProductoAPI(producto) {
+
+    try {
+        console.log("enviando producto")
+
+        const respuesta = await fetch(API_URL, {
+
+            method: "POST",
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            },
+
+            body: JSON.stringify(producto)
+
+        });
+
+        const datos = await respuesta.json();
+
+        console.log("POST:", datos);
+
+    } catch (error) {
+
+        console.error("Error POST:", error);
+
+    }
+}
+
+
+
+async function actualizarProductoAPI(id, productoActualizado) {
+
+    try {
+
+        const respuesta = await fetch(`${API_URL}/${id}`, {
+
+            method: "PUT",
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            },
+
+            body: JSON.stringify(productoActualizado)
+
+        });
+
+        const datos = await respuesta.json();
+
+        console.log("PUT:", datos);
+
+    } catch (error) {
+
+        console.error("Error PUT:", error);
+
+    }
+}
+
+
+
+
+async function eliminarProductoAPI(id) {
+
+    try {
+
+        const respuesta = await fetch(`${API_URL}/${id}`, {
+
+            method: "DELETE"
+
+        });
+
+        console.log("DELETE:", respuesta);
+
+    } catch (error) {
+
+        console.error("Error DELETE:", error);
+
+    }
+}
+
+
 
 syncBtn.addEventListener("click", () => {
 
